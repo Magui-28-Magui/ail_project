5a633c93a45157a05d17afc1a4f959ee0863fe69
Magui Campos <88505411+MaguiCampos@users.noreply.github.com>
2022-07-15 10:45:04 -0700

    add url server

--- ail_2022/backend/application/config/constants.php
+++ ail_2022/backend/application/config/constants.php
@@ -85,4 +85,4 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 
-defined('SERVER_PATH_URL')     OR define('SERVER_PATH_URL', 'http://localhost/');
+defined('SERVER_PATH_URL')     OR define('SERVER_PATH_URL', 'http://mxmtsvrandon1/');
